9186a14d2de804ba6c64de699ec2003888297092
Q. Price <priceqp@gmail.com>
2019-05-26 15:21:04 -0500

    Add Level 1 Auth - Username and Password Only

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const express = require("express");
 const ejs = require("ejs");
 
@@ -9,6 +10,14 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
   res.render("home");
@@ -20,6 +29,43 @@ app.get("/login", function(req, res){
 
 app.get("/register", function(req, res){
   res.render("register");
+});
+
+
+app.post("/register", function(req, res){
+
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+
+});
+
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    } else {
+      if(foundUser) {
+        if(foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+
+
 });
 
 
